9180d4c435ed245ca1c8590a521806d65686bfc2
alimaqbool2122 <alimaqbool2122@gmail.com>
2023-09-06 22:34:10 +0500

    Educational Website
    Educational  Responsiveness Website
    for all School , Colleges and University.

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+        // Script for navigation bar
+        var navlinks = document.getElementById("navbar");
+        function ShowMenu(){
+            navlinks.style.right = "0";
+        }
+        function hideMenu(){
+            navlinks.style.right = "-250px";
+        }
